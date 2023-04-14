e76af971c3794285fd8f6b5a7bc6c7e231eb7895
Alex Guerra <alexguerra618@gmail.com>
2023-04-14 06:00:17 -0400

    poisson and OnOff generation implemented.

--- src/data_gen.py
+++ src/data_gen.py
@@ -18,21 +18,27 @@ from ns.switch.switch import SimplePacketSwitch
 from ns.switch.switch import FairPacketSwitch
 from ns.topos.fattree import build as build_fattree
 from ns.topos.utils import generate_fib, generate_flows, read_topo
+from ns.utils.generators.pareto_onoff_generator import pareto_onoff_generator
+from ns.utils.generators.MAP_MSP_generator import BMAP_generator
 
 from data_gen_utils import flow_to_df, merge_flow_dfs
 
 parser = argparse.ArgumentParser(description='Data Generation Script for DeepQueueNet using ns.py')
 
 parser.add_argument('--port-rate', type=int, default=1024 * 1024 * 10, 
-                    help='Port Rate in bytes/second (default: 10 Mb/s)')
-parser.add_argument('--buffer-size', type=int, default=1024 * 1024,
-                    help='Buffer Size in bytes (default: 1 Mb)')
+                    help='Port Rate in bps (default: 10 Mbs)')
+parser.add_argument('--buffer-size', type=int, default=1024 * 1024, # TODO: What is a realistic number for this?
+                    help='Buffer Size in bytes (default: 1 MB)')
+parser.add_argument('--mean-pkt-size', type=int, default=1000,
+                    help='Mean packet size in bytes (default: 1000 bytes)')
 parser.add_argument('--scheduler', type=str, default='FIFO', choices=['DRR', 'WFQ', 'SP', 'FIFO'],
                     help='which scheduler to use. (options: DRR, WFQ, SP, FIFO  default: FIFO)')
+parser.add_argument('--traffic-gen', type=str, default='Poisson', choices=['Poisson', 'OnOff', 'MAP'],
+                    help='Which traffic generator to use for hosts. (options: Poisson, OnOff, MAP  default: Poisson)')
 parser.add_argument('--num-ports', type=int, default=4,
                     help='Max number of ports in the switch (default: 4)')
 parser.add_argument('--num-flows', type=int, default=20,
-                    help='Number of flows to use in the simulation (default: 5)')
+                    help='Number of flows to use in the simulation (default: 20)')
 parser.add_argument('--duration', type=float, default=1000.,
                     help='Number of seconds to run the simulation (default: 1000)')
 parser.add_argument('--output-dir', type=str, default='data',
@@ -41,25 +47,57 @@ parser.add_argument('--output-name', type=str, default='rsim',
                     help='Name of the output csv file (default: rsim)')
 args = parser.parse_args()
 
+def interarrival(y):
+    try:
+        return next(y)
+    except StopIteration:
+        return
+    
+def packet_size():
+    # TODO: Add options for a wider range of packet size distributions
+    return 1 + int(np.random.poisson(args.mean_pkt_size))
+
 def generate_synthetic_traffic_dataset(G, all_flows):
 
     # Create the environment
     env = simpy.Environment()
 
-    # TODO: parameterize this better.
-    mean_pkt_size = 1000.0
-    # Generate a distribution of packet sizes.
-    size_dist = partial(expovariate, 1.0 / mean_pkt_size)
-    # Use the poisson process to specify packet generation. 
-    arr_dist = partial(expovariate, 1 + np.random.rand())
+    # Here we setup various ways of generating traffic.
+    if args.traffic_gen == 'Poisson':
+        packets_per_second = args.port_rate / args.mean_pkt_size * 8 # packets per second
+        lambd = 0.5 * packets_per_second # TODO: Vary the link load between 0.1 and 0.9
+        iat_dist = partial(expovariate, lambd)
+
+    elif args.traffic_gen == 'OnOff': 
+            
+        y = pareto_onoff_generator(on_min=0.5, # This value is from the paper
+                                on_alpha=1.5,
+                                off_min=0.2, # This value is from the paper
+                                off_alpha=1.5,
+                                on_rate=args.port_rate,
+                                pktsize=args.mean_pkt_size)
+
+        iat_dist = partial(interarrival, y)
+
+    elif args.traffic_gen == 'MAP':
+        # TODO This is complicated, because I need to setup the interarrival matrices D0 and D1.
+        # This will look something like:
+        # D0 = np.array([[-114.46031, 11.3081, 8.42701],
+        #            [158.689, -29152.1587, 20.5697],
+        #            [1.08335, 0.188837, -1.94212]])
+        # D1 = np.array([[94.7252, 0.0, 0.0], [0.0, 2.89729e4, 0.0],
+        #             [0.0, 0.0, 0.669933]])
+        # y = BMAP_generator([D0, D1])
+        # iat_dist = partial(interarrival, y)
+        raise NotImplementedError("MAP generation is not implemented yet")
 
     # Setup the simpy components the proper way
     for fid in all_flows:
         
         pg = DistPacketGenerator(env,
                                 f"Flow_{fid}",
-                                arr_dist,
-                                size_dist,
+                                iat_dist,
+                                packet_size,
                                 flow_id=fid)
         ps = PacketSink(env)
 
