0c8d135f0479c4f35a39ef9c329a5f5bf66af43b
Christina Shatford <cshatfor@conncoll.edu>
2023-04-27 20:05:59 -0400

    output csv with ts, pred etime, cur_hub, and priority

--- src/predict.py
+++ src/predict.py
@@ -8,25 +8,33 @@ import argparse
 import json
 
 from dataset import HistogramTracesDataset
-from dataset import TracesDataset 
 from ptm import deepPTM, load_model_from_ckpt
 
-def predict(model, save_csv_path):    
+def predict(model, csv_paths, save_csv_path):    
     model.eval()
+    
 
     # Create data structures to store all data
-    features = [] # we aren't currently including features in the generated csv
+    timestamps = []
     predictions = []
-    labels = []
+    cur_hubs = []
+    priorities = []
+    # labels = [] # we aren't currently including ground truth in the generated csv
     with torch.no_grad():
         # iterate through data loader
-        for batch_x, batch_y in dataloader:            
+        for batch_x, batch_y, batch_cur_hubs in dataloader:            
             # Move to devices
             batch_x= batch_x.to(device)
             batch_y = batch_y.to(device)
 
-            # features.append(batch_x)
-            labels.append(batch_y)
+            # expand the cur_hub from (batches) to (batches, n_timesteps)
+            temp = batch_cur_hubs.clone().unsqueeze(1).expand(-1, n_timesteps)
+
+            # extract the features we want to include in our histograms later
+            cur_hubs.append(temp.cpu().numpy())
+            priorities.append((batch_x[:,:,3].clone().cpu().numpy()).astype(int))
+            timestamps.append(batch_x[:,:,0].clone().cpu().numpy())
+            # batch_x[:,:,0] is packet_length
 
             # Predict
             batch_predictions = model(batch_x)
@@ -36,15 +44,26 @@ def predict(model, save_csv_path):
             predictions.append(batch_predictions)
 
         # Concatenate into single arrays
-        # features = np.concatenate(features, axis=0)
+        timestamps = np.concatenate(timestamps, axis=0)
+        # print(timestamps.shape)
+        cur_hubs = np.concatenate(cur_hubs, axis=0)
+        priorities = np.concatenate(priorities, axis=0)
         predictions = np.concatenate(predictions, axis=0)
-        predictions = predictions.flatten() # flatten so each row is per-packet instead of per-batch
-        labels = np.concatenate(labels, axis=0)
-        labels = labels.flatten()
+
+
+
+        # flatten so each row is per-packet instead of per-batch
+        timestamps = timestamps.flatten()
+        cur_hubs = cur_hubs.flatten()
+        priorities = priorities.flatten()
+        predictions = predictions.flatten() 
+        # labels = np.concatenate(labels, axis=0)
+        # labels = labels.flatten()
 
         # Create data frame
-        df = pd.DataFrame(list(zip(labels, predictions)),
-               columns =['actual', 'predictions'])   
+        df = pd.DataFrame(list(zip(timestamps, predictions, cur_hubs, priorities)),
+               columns =['timestamp', 'etime', "cur_hub", "priority"])   
+        # save to csv
         df.to_csv(save_csv_path)
 
     
@@ -67,11 +86,11 @@ if __name__ == "__main__":
 
     # csv_paths points to processed validation data
     csv_paths = [data_specs["val_data_pth"]] if not isinstance(data_specs["val_data_pth"], list) else data_specs["val_data_pth"]
-    
     # Create data set
     dataset = HistogramTracesDataset(
         csv_paths=csv_paths,
         n_timesteps=n_timesteps)
+    
  
     # Create data loader
     dataloader = torch.utils.data.DataLoader(
@@ -79,7 +98,8 @@ if __name__ == "__main__":
         shuffle=False,
         batch_size=64,
         num_workers=8)
-
+  
+    
     model = deepPTM(in_feat=dataset.num_feat,
                     lstm_config=model_specs["lstm_config"],
                     attn_config=model_specs["attn_config"],
@@ -94,7 +114,7 @@ if __name__ == "__main__":
         saved_model_pth = specs['trained_model_pth']
         load_model_from_ckpt(model, specs['trained_model_pth'])
 
-    predict(model, save_csv_path)
+    predict(model, csv_paths, save_csv_path)
 
     
 
